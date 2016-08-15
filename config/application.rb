3095ea53a416a92c0b7663e83fe38cd9d0d40671
estevanmontoya <montoya.estevan@gmail.com>
2016-08-15 14:11:05 -0600

    Added Devise Gem for user registration

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Pinteresting
+module SermonStation
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+      # Required for Heroku
+      config.assets.initialize_on_precompile = false
   end
 end
